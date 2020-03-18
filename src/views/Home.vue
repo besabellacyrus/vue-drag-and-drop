1a009eb49a04f3a56351d54fa68a53289d14ef5e
Cyrus <besabellacyrus@gmail.com>
2020-03-18 21:18:31 +0800

    done initial prototype for drag and drop items on the list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="list-wrapper">
+    <div
+      class="item-wrapper"
+      v-for="(company, index) in computedCompanies"
+      :key="index"
+      draggable="false"
+    >
+      <h1>{{ company.name }}</h1>
+      <h3>{{ company.position }}</h3>
+      <p>{{ company.desc }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      companies: [
+        {
+          name: 'FDM',
+          position: 'Full Stack Web Developer',
+          desc: 'Create web based games using Vue Js and websocket'
+        },
+        {
+          name: 'Vervespire Tech Inc',
+          position: 'Web Developer',
+          desc: 'API creation for the Mobile app and Responder App API and CMS creation for the LGU'
+        },
+        {
+          name: 'Mobext Ph',
+          position: 'Web Developer',
+          desc: 'Web Developer'
+        }
+      ],
+      draggedIndex: null,
+      dragOverIndex: null
+    }
+  },
+  computed: {
+    computedCompanies () {
+      return this.array_move(this.companies, this.draggedIndex, this.dragOverIndex)
+    }
+  },
+  mounted () {
+    const items = document.querySelectorAll('.item-wrapper')
+
+    items.forEach((e, index) => {
+      e.ondragstart = () => {
+        this.draggedIndex = index
+      }
+      e.onmousedown = (event) => {
+        e.setAttribute('draggable', true)
+        console.log('mousedown')
+      }
+      e.ondragover = () => {
+        console.log('dragover', index)
+        this.dragOverIndex = index
+      }
+      e.ondragend = () => {
+        e.setAttribute('draggable', false)
+      }
+    })
+  },
+  methods: {
+    array_move (arr, oldIndex, newIndex) {
+      if (newIndex >= arr.length) {
+        var k = newIndex - arr.length + 1
+        while (k--) {
+          arr.push(undefined)
+        }
+      }
+      arr.splice(newIndex, 0, arr.splice(oldIndex, 1)[0])
+      return arr
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.list-wrapper {
+  width: 450px;
+  .item-wrapper {
+    padding: 1rem;
+    margin-bottom: 1rem;
+    &:hover {
+      background-color: #fff4d3;
+    }
+    h1 {
+      font-size: 25px;
+    }
+  }
+}
+</style>
